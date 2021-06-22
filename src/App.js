e4b52dd9d061d6a6ef6284814e6b2c40a2d31565
waleed shaikh <waleedshykh5@gmail.com>
2021-06-22 18:00:19 +0500

    WORK DONE

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './screen/home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
